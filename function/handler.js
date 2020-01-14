00930326e9d006e4fbf5ae66a8fae5dc9e3f2430
Jérôme Pogeant <p-jerome@hotmail.fr>
2020-01-14 14:57:41 +0100

    Use Openfaas function structure

---
+++ function/handler.js
@@ -0,0 +1,24 @@
+import fetch from 'node-fetch';
+import { dialogflow, SimpleResponse } from 'actions-on-google';
+
+const app = dialogflow({ debug: true });
+
+async function checkStations() {
+  return fetch('https://opendata.lillemetropole.fr/api/v2/catalog/datasets/vlille-realtime/records')
+    .then((res) => res.json())
+    .then((json) => json.records[0].record.fields.nbvelosdispo)
+    .catch((err) => console.log(err));
+}
+
+module.exports = async () => {
+  app.intent("Vérifier la disponibilité des V'Lille", async (conv) => {
+    const bikeNumber = await checkStations();
+
+    conv.close(new SimpleResponse({
+      text: `Il y a ${bikeNumber} velos.`,
+      speech: `Il y a ${bikeNumber} velos dans la station la plus proche.`,
+    }));
+  });
+
+  return app;
+};
