bd2b6cd5fc376f5500377b4bd64bab00445be697
Jérôme Pogeant <p-jerome@hotmail.fr>
2020-01-14 16:00:51 +0100

    Fix GitHub action workflows configuration

--- function/handler.js
+++ function/handler.js
@@ -1,24 +1,51 @@
 const fetch = require('node-fetch');
-const { dialogflow, SimpleResponse } = require('actions-on-google');
-
-const app = dialogflow({ debug: true });
 
 async function checkStations() {
-  return fetch('https://opendata.lillemetropole.fr/api/v2/catalog/datasets/vlille-realtime/records')
+  return await fetch('https://opendata.lillemetropole.fr/api/v2/catalog/datasets/vlille-realtime/records')
     .then((res) => res.json())
     .then((json) => json.records[0].record.fields.nbvelosdispo)
     .catch((err) => console.log(err));
 }
 
-module.exports = async () => {
-  app.intent("Vérifier la disponibilité des V'Lille", async (conv) => {
-    const bikeNumber = await checkStations();
+module.exports = (context, callback) => {
+  const input = JSON.parse(context);
+  const responseObject = {
+    fulfillmentText: "This is a text response",
+    fulfillmentMessages: [],
+    source: "V'lille assistant",
+    payload: {
+      google: {
+        expectUserResponse: true,
+        richResponse: {
+          items: [
+            {
+              simpleResponse: {}
+            }
+          ]
+        }
+      }
+    }
+  }
 
-    conv.close(new SimpleResponse({
-      text: `Il y a ${bikeNumber} velos.`,
-      speech: `Il y a ${bikeNumber} velos dans la station la plus proche.`,
-    }));
-  });
+  if (input && input.queryResult) {
+    switch (input.queryResult.intent.displayName) {
+      case 'welcome':
+        responseObject.payload.google.richResponse.items[0].simpleResponse.textToSpeech = "Bonjour, que voulez-vous demander à l'assistant V'lille ?";
+        break;
+      case "disponibility":
+        const bikes = checkStations();
+        responseObject.payload.google.richResponse.items[0].simpleResponse.textToSpeech = `Il y a ${bikes} vélos disponible.`;
+        break;
+      case 'input.unknown':
+      case 'default':
+      default:
+        responseObject.payload.google.richResponse.items[0].simpleResponse.textToSpeech = 'Raté ! Je ne comprend pas ce que vous dites.';
+    }
 
-  return app;
+    responseObject.fulfillmentText = responseObject.payload.google.richResponse.items[0].simpleResponse.textToSpeech;
+  } else {
+    responseObject.payload.google.richResponse.items[0].simpleResponse.textToSpeech = 'Unable to parse the input. Function may be out of date';
+  }
+
+  callback(undefined, responseObject);
 };
